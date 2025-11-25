f16b939816be818098887850d74c0b2ec726c56a
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-11-26 00:28:29 +0300

    Update payment.js

--- api/payment.js
+++ api/payment.js
@@ -1,1 +1,10 @@
+const { handlePaymentWebhook } = require("../index.js");
+
+module.exports = async (req, res) => {
+  if (req.method === "POST") {
+    return handlePaymentWebhook(req, res);
+  }
+
+  res.status(200).send("Юкасса Webhook активен");
+};
 
