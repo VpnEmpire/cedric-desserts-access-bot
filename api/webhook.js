4026bbd09ec8271aba2f60b784f0635ed59a62c8
VpnEmpire <zarema.abdurashidova00@mail.ru>
2025-11-26 23:41:53 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,17 +1,15 @@
 const { handleUpdate } = require("../index.js");
 
 module.exports = async (req, res) => {
-  if (req.method === "POST") {
-    try {
+  try {
+    if (req.method === "POST") {
       await handleUpdate(req.body);
-      res.status(200).json({ ok: true });
-    } catch (error) {
-      console.error("Ошибка в webhook:", error);
-      res.status(500).json({ ok: false });
+      return res.status(200).json({ ok: true });
     }
-  } else {
-    res
-      .status(200)
-      .send("Cedric desserts bot is running. Webhook работает.");
+
+    return res.status(200).send("Cedric desserts bot is running.");
+  } catch (err) {
+    console.error("Ошибка в webhook:", err);
+    res.status(500).json({ ok: false });
   }
 };
